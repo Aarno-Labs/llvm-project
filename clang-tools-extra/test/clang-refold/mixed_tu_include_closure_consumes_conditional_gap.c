d9b6f54f94c9a1a0b7d2b8fdbd9490de52c64372
Jordan Eikenberry <jeikenberry@aarno-labs.com>
2026-05-13 16:14:25 -0400

    Renamed a couple of llvm-lit tests from `*_rejects_*` to `*_consumes_*`, since these tests are actually now refoldable and do not simply resolve into the raw B-side bytes.

--- clang-tools-extra/test/clang-refold/mixed_tu_include_closure_consumes_conditional_gap.c
+++ clang-tools-extra/test/clang-refold/mixed_tu_include_closure_consumes_conditional_gap.c
@@ -1,4 +1,4 @@
-// RUN: %clang-refold-tester mixed_tu_include_closure_rejects_conditional_gap
+// RUN: %clang-refold-tester mixed_tu_include_closure_consumes_conditional_gap
 #define KEEP(x) ((x) + 1)
 
 int untouched = KEEP(5);
